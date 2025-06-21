144a6c9f9c91dfee08678bb9219191a906331c05
akash07j <av4456ts@gmail.com>
2025-06-21 21:12:15 +0530

    BS

---
+++ src/main/java/SimpleBook/BookSimple/BookSimpleApplication.java
@@ -0,0 +1,13 @@
+package SimpleBook.BookSimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSimpleApplication.class, args);
+	}
+
+}
